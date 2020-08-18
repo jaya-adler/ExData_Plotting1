45542ab97a21eecb660c9f79b2b9549310c25223
jaya-adler <jayashankar9741@gmail.com>
2020-08-18 08:56:30 +0530

    added detail of xy

--- plot3.R
+++ plot3.R
@@ -28,5 +28,6 @@ plot(dt,sub1, ylab = "Energy Sub metering",xlab = "", type = "n")
 points(x = dt,y = sub1, type = "l")
 points(x = dt,y = sub2, type = "l", col = "red")
 points(x = dt,y = sub3, type = "l", col = "blue")
+legend ("topright", lty=1, col=c("black", "red","blue"), legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 dev.off()
 
